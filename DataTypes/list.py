611c262e7de65a8061f2766ce39a5d9cc7a2e783
Jayaram_dev <jayaramvenkatanarayanan@live.com>
2020-06-07 17:48:21 +0000

    List builtin function (29) completed

--- DataTypes/list.py
+++ DataTypes/list.py
@@ -22,3 +22,68 @@ print(my_list2)
 print('copyList')
 my_list_copy = my_list1[:]
 print(my_list_copy)
+
+#matrix
+my_matrix =[
+  [1,0,9],
+  [2,46,5],
+  [5,66,6]
+]
+
+print(my_matrix[1][1])
+
+
+# using this list: 
+basket = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+'''find Oranges'''
+print('''find Oranges''')
+print(basket[1][1][0])
+
+
+
+#Bulit in list function 
+sample_list =[1,2,3,4,5]
+
+#append add the value in last 
+sample_list.append(100)
+my_list = sample_list
+print(my_list)
+print('----------------')
+#add value with Index
+
+sample_list.insert(3,300)
+index_val = sample_list
+print(index_val)
+print('----------------')
+
+#extends
+index_val.extend([101,109])
+my_ext = index_val
+print(my_ext)
+print('----------------')
+
+#pop normal 
+print(my_ext.pop())
+print(my_ext)
+print('----------------')
+
+#pop with index 
+print(my_ext.pop(3))
+print(my_ext)
+print('----------------')
+
+#remove the Value
+my_ext.remove(5)
+print(my_ext)
+print('----------------')
+
+#copy 
+my_listCopy = my_ext.copy()
+print(my_listCopy)
+print('----------------')
+
+#clear
+my_ext.clear()
+print(my_listCopy)
+print(my_ext)
+print('----------------')
